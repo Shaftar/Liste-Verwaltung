e17167879437c7739b2f663f7cad51ae739e75fb
Mohamad Shaftar <47214343+Shaftar@users.noreply.github.com>
2019-10-29 16:05:41 +0100

    Update Aufgabe2-Liste-Verwaltung.py

--- Aufgabe2-Liste-Verwaltung.py
+++ Aufgabe2-Liste-Verwaltung.py
@@ -1,6 +1,7 @@
 from termcolor import colored
 liste = []
 anfrage = str
+hallotext = colored("To close  'Enter' ", 'magenta')
 print("Hallo, was möchtest du tun?")
 print("a) Liste anzeigen")
 print("b) Listenelemente hinten anfügen")
@@ -8,7 +9,7 @@ print("c) Listenelement an einer bestimmten Stelle einfügen")
 print("d) Listenelement löschen")
 print("e) Suchen (Vorhandensein bestätigen und Index eines Elementes ausgeben)")
 print("Bitte geben Sie einen Buchstaben von (a,b,c,d,e) ein")
-print("To close  'Enter' ")
+print(hallotext)
 print()
 while True:
     print()
@@ -18,6 +19,7 @@ while True:
             text = colored('List is empty', 'red')
             print(text)
         else:
+            print('list contains: ... ', end='')
             for i in liste:
                 print(i + " ", end=" ")
     elif eingabe == 'b':
@@ -36,11 +38,17 @@ while True:
             eingabe2 = input(text1)
             if eingabe2 != '':
                 print("Index of list " + str(len(liste)))
-                text2 = colored("pleas enter index of element: ", 'orange')
-                zahl = int(input(text2))
+                text2 = colored("pleas enter index of element: ", 'yellow')
+                zahl = int
+                try:
+                    zahl = int(input(text2))
+                except ValueError:
+                    print('PLease input a valid integer')
+                boolwert = False
                 if zahl <= len(liste):
-                    #liste.remove(liste[zahl])
-                    liste.append(eingabe2[zahl])
+                    liste.insert(zahl, eingabe2)
+                    text7 = colored(" added to list! ", 'green')
+                    print(eingabe2 + text7)
                 else:
                     text3 = colored("Index out of range", 'red')
                     print(text3)
@@ -52,18 +60,41 @@ while True:
         while anfrage != '':
             text4 = colored("pleas enter element to delet: ", 'blue')
             eingabe3 = input(text4)
+            boolwert1 = False
             if eingabe3 != '':
-                liste.remove(eingabe3)
+                for i in liste:
+                    if eingabe3 == i:
+                        boolwert1 = True
+                if boolwert1:
+                    liste.remove(eingabe3)
+                    print(eingabe3 + ' has been removed!')
+                else:
+                    text6 = colored("Element is not in list!", 'red')
+                    print(text6)
+                    pass
             else:
                 break
     elif eingabe == 'e':
-        print(".................to exit 'Enter'")
+        print("..................................to exit 'Enter'")
         while anfrage != '':
-            text5 = colored('please enter element to show index: ')
+            text5 = colored('please enter element to show index: ', 'blue')
             eingabe4 = input(text5)
-            liste.index(eingabe4)
-            if eingabe4 == '':
-                liste.remove('')
+            boolwert2 = bool
+            if eingabe4 != '':
+                for i in liste:
+                    if eingabe4 == i:
+                        boolwert2 = True
+                        break
+                    else:
+                        boolwert2 = False
+                        pass
+                if not boolwert2:
+                    text3 = colored("Element is not in list!", 'red')
+                    print(text3)
+                    pass
+                else:
+                    print(str(liste.index(eingabe4)))
+            else:
                 break
     else:
         exit()
